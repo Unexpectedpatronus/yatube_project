3f02c32cc08b7a63f8fc15f579bed216b23baaa8
Evgeny Odintsov <ev1genial@gmail.com>
2022-11-12 03:00:37 +0300

    Добавил словарь context в функцию view

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,10 +4,18 @@ from django.http import HttpResponse
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'text': text
+    }
+    return render(request, template, context)
 
 
 # В урл мы ждем парметр, и нужно его прередать в функцию для использования
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    return render(request, template)
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text
+    }
+    return render(request, template, context)
