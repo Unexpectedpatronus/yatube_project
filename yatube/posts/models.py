6da4502a451cbd74d1f654113de4e134b8ceafb0
Evgeny Odintsov <ev1genial@gmail.com>
2022-11-12 14:35:48 +0300

    Добавил модель Post

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
